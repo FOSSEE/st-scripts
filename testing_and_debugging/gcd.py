7115a52508ea7aa4373b6ddeadc7ea355b63c7ab
Jovina <jovinadsouza@gmail.com>
2011-07-19 16:25:20 +0530

    3 files added to 'testing and debugging'.

---
+++ testing_and_debugging/gcd.py
@@ -0,0 +1,10 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a%b)
+
+if __name__ == '__main__':
+    result = gcd(48, 64)
+        if result != 16:
+            print "Test failed"
+        print "Test Passed"
